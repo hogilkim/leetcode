f1ddec820d4598dc1f6c54a2e8100112aea82888
Hogyeong Kim <hogilkim@gmail.com>
2021-10-28 21:45:02 -0400

    twosome re-practice

--- 1 TwoSome.py
+++ 1 TwoSome.py
@@ -1,15 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         seenMap = {}
         
-        for i, value in enumerate(nums):
+        for index, value in enumerate(nums):
             diff = target - value
             if diff in seenMap:
-                return [seenMap[diff], i]
-            seenMap[value] = i
-        return
+                return [seenMap[diff], index]
+            seenMap[value] = index
+            
