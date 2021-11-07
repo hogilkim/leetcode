88d2245ad6a06be46c3ed41487ccfa56520a3fbd
Hogyeong Kim <hogilkim@gmail.com>
2021-11-07 13:28:49 -0500

    Nov 7, 2021

---
+++ 100. Same Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        return self.observer(p, q)
+    
+    def observer(self, p, q):
+        if not p and not q:
+            return True
+        elif not p and q:
+            return False
+        elif p and not q:
+            return False
+        else:
+            if p.val != q.val:
+                return False
+        return self.observer(p.left, q.left) and self.observer(p.right, q.right)
