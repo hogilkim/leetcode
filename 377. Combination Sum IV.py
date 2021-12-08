234d1e469124606f04b29236f1aedf459f4f88c9
Hogyeong Kim <hogilkim@gmail.com>
2021-12-07 19:15:08 -0500

    Dec 7, 2021 377

---
+++ 377. Combination Sum IV.py
@@ -0,0 +1,11 @@
+# try again
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        dp = [0 for i in range(target+1)]
+        dp[0]=1
+        for i in range(1,len(dp)):
+            for num in nums:
+                if i-num >=0:
+                    dp[i]+= dp[i-num]
+        return dp[-1]
