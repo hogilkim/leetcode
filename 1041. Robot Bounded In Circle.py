4fb336303294001bb6f493fe4ffd6fb271823c9b
Hogyeong Kim <hogilkim@gmail.com>
2021-12-16 09:59:33 -0500

    Dec 16, 2021 1041

---
+++ 1041. Robot Bounded In Circle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        dir_x , dir_y = 0, 1
+        x, y = 0, 0
+        
+        for inst in instructions:
+            if inst == "G":
+                x += dir_x
+                y += dir_y
+            elif inst == "L":
+                dir_x, dir_y = -1*dir_y, dir_x
+            else:
+                dir_x, dir_y = dir_y, -1*dir_x
+        
+        return (x,y) == (0,0) or (dir_x, dir_y) != (0, 1)
