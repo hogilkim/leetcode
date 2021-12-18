268567fd33ddb7ed322114409edd9b9a0975e03e
Hogyeong Kim <hogilkim@gmail.com>
2021-12-18 15:34:05 -0500

    Dec 18, 2021 926

---
+++ 926. Flip String to Monotone Increasing.py
@@ -0,0 +1,27 @@
+# solve again
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        count0 = s.count('0')
+        count1 = 0
+        res = len(s) - count0
+        
+        for char in s:
+            if char == '0':
+                count0 -= 1
+            else:
+                res = min(res, count0 + count1)
+                count1 += 1
+        return res
+    
+    
+    
+        # simple, but not intuitive solution
+#         min_flips = flips = s.count('0')
+        
+#         for char in s:
+#             if char == '0':
+#                 flips -= 1
+#             else:
+#                 flips += 1
+#             min_flips = min(min_flips, flips)
+#         return min_flips
