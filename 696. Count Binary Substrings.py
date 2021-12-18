d7b22e99331be56ff0d9a36fc82f87eff97b6a7f
Hogyeong Kim <hogilkim@gmail.com>
2021-12-17 23:11:11 -0500

    Dec 17, 2021 696

---
+++ 696. Count Binary Substrings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBinarySubstrings(self, s: str) -> int:
+        # solve again
+        curr, prev, ans = 1, 0, 0
+        
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]: curr += 1
+            else:
+                ans += min(curr, prev)
+                prev, curr = curr, 1
+        
+        return ans + min(prev,curr)
