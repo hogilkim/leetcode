d93bff06b7bc1e9c299b15b9ecf2c22e1da38a67
Hogyeong Kim <hogilkim@gmail.com>
2021-12-23 22:11:27 -0500

    Dec 23, 2021 112-2

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -6,15 +6,27 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # second solution
+        if not root: return False
         
-        def dfs(curr_node, total_sum):
-            if not curr_node:
-                return False
-            total_sum += curr_node.val
-            print(total_sum)
-            if total_sum == targetSum:
-                if not curr_node.left and not curr_node.right:
-                    return True
-            return dfs(curr_node.left, total_sum) or dfs(curr_node.right, total_sum)
+        if not root.left and not root.right and targetSum == root.val:
+            return True
+        
+        targetSum -= root.val
+        
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+        
+        # first solution
+# class Solution:
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         def dfs(curr_node, total_sum):
+#             if not curr_node:
+#                 return False
+#             total_sum += curr_node.val
+#             print(total_sum)
+#             if total_sum == targetSum:
+#                 if not curr_node.left and not curr_node.right:
+#                     return True
+#             return dfs(curr_node.left, total_sum) or dfs(curr_node.right, total_sum)
             
-        return dfs(root, 0)
+#         return dfs(root, 0)
