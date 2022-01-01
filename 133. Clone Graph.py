0f2969cf7075807f2d1cc5f290e8358e7162cd07
Hogyeong Kim <hogilkim@gmail.com>
2022-01-01 16:18:32 -0500

    Jan 1, 2022 133-2

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -1,3 +1,20 @@
+# second try, couldn't solve
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        
+        og_to_clone = {}
+        
+        def dfs(node):
+            if node in og_to_clone:
+                return og_to_clone[node]
+            copy = Node(node.val)
+            og_to_clone[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return copy
+        return dfs(node) if node else None
+
 # Solve again
 
 class Solution:
