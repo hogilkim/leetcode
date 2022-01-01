a73b8b973c8f122d19f186f4d0387db3ed2f7d0e
Hogyeong Kim <hogilkim@gmail.com>
2022-01-01 15:42:16 -0500

    Jan 1, 2022 5-2

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -12,3 +12,35 @@ class Solution:
             l -= 1
             r += 1
         return s[l+1:r]
+
+
+# Solved, but previous solution is much better
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        max_len = 0
+        max_l = len(s)
+        max_r = 0
+        for i in range(len(s)):
+            l1, r1 = self.longest_palin(s, i, i)
+            if r1 - l1 + 1> max_len:
+                max_len = r1-l1+1
+                max_l = l1
+                max_r = r1
+            l2, r2 = self.longest_palin(s, i, i+1)
+            if r2 - l2 + 1 > max_len:
+                max_len = r2-l2+1
+                max_l = l2
+                max_r = r2
+        return s[max_l:max_r + 1]
+        
+        
+        
+    def longest_palin(self, s, l, r):
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                return l+1, r-1
+            l -= 1
+            r += 1
+        return l+1, r-1
+        
+        
