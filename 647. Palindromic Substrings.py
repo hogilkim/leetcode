f75eb3cf0ac2da98f6bd396f9d2c7fe2c8fb3b5d
Hogyeong Kim <hogilkim@gmail.com>
2022-01-03 21:23:26 -0500

    Jan 3, 2022 647-2

--- 647. Palindromic Substrings.py
+++ 647. Palindromic Substrings.py
@@ -1,3 +1,27 @@
+# second try - solved
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        total_palin = 0
+        for i in range(len(s)):
+            total_palin += self.helper(s,i,i) + self.helper(s,i,i+1)
+        
+        return total_palin
+        
+        
+    def helper(self, s, l, r):
+        if r >= len(s): return 0
+        
+        palin_num = 0
+        while l>=0 and r < len(s):
+            if s[l] == s[r]:
+                palin_num += 1
+            else:
+                break
+            l-=1
+            r+=1
+        return palin_num
+
+        
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0
