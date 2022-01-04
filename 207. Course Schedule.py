b64d8fb4dbe603dc5795cfc7a0611c3dbf9334dd
Hogyeong Kim <hogilkim@gmail.com>
2022-01-04 15:26:54 -0500

    Jan 4, 2022 207-2

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,3 +1,33 @@
+# second attempt - solved!
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        prereqs = {num:[] for num in range(numCourses)}
+        
+        for course, prereq in prerequisites:
+            prereqs[course].append(prereq)
+        
+        visited = set()
+        path = set()
+        def dfs(course):
+            if course in path: return False
+            
+            path.add(course)
+            
+            
+            for prereq in prereqs[course]:
+                if prereq not in visited:
+                    if not dfs(prereq):
+                        return False
+                    
+            path.remove(course)
+            visited.add(course)
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+        return True
+
 #Try Again
 
 class Solution:
