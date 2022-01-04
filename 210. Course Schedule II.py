f957948b3a4b2c3a6a422c481d5c6c0f1a86a66c
Hogyeong Kim <hogilkim@gmail.com>
2022-01-04 15:48:04 -0500

    Jan 4, 2022 210-2

--- 210. Course Schedule II.py
+++ 210. Course Schedule II.py
@@ -1,3 +1,33 @@
+# second try - solved!
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        prereqs = {num:[] for num in range(numCourses)}
+        
+        for a,b in prerequisites:
+            prereqs[a].append(b)
+            
+        path = set()
+        visited = set()
+        res = []
+        
+        def dfs(course):
+            if course in path: return False
+            elif course in visited: return True
+
+            
+            path.add(course)
+            
+            for prereq in prereqs[course]:
+                if not dfs(prereq): return False
+            
+            res.append(course)
+            path.remove(course)
+            visited.add(course)
+            
+            return True
+        
+        return res if all(dfs(course) for course in range(numCourses)) else []
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         prereq_map = {i:[] for i in range(numCourses)}
