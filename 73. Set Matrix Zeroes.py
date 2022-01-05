3ce543efeff783a755cfcecdda7432f99dbe5e55
Hogyeong Kim <hogilkim@gmail.com>
2022-01-05 10:41:04 -0500

    Jan 5, 2022 73-2

--- 73. Set Matrix Zeroes.py
+++ 73. Set Matrix Zeroes.py
@@ -1,3 +1,37 @@
+#second solution
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        ROW, COL = len(matrix), len(matrix[0])
+        
+        set_to_zero = set()
+        
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+        
+        def dfs(row, col, row_dir, col_dir):
+            if row >= ROW or col >= COL or row < 0 or col < 0:
+                return
+            
+            
+            if row_dir == 0 and col_dir == 0 and matrix[row][col] == 0 and (row,col) not in set_to_zero:
+                for dir_row, dir_col in directions:
+                    dfs(row+dir_row, col+dir_col, dir_row, dir_col)
+                
+                
+            elif row_dir != 0 or col_dir != 0:
+                if matrix[row][col] != 0:
+                    set_to_zero.add((row,col))
+                    matrix[row][col] = 0
+                
+                dfs(row+row_dir, col+col_dir, row_dir, col_dir)
+                
+            
+        for row in range(ROW):
+            for col in range(COL):
+                dfs(row,col,0,0)
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
