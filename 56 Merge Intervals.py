55d9ed56dc9e595611b1f713ed646302abfcf929
Hogyeong Kim <hogilkim@gmail.com>
2022-01-07 13:08:10 -0500

    jan 7, 2022 56-2

--- 56 Merge Intervals.py
+++ 56 Merge Intervals.py
@@ -1,3 +1,23 @@
+# second try Jan 7, 2022 solved!
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        intervals = sorted(intervals, key=lambda x:x[1])
+        intervals = sorted(intervals, key=lambda x:x[0])
+        
+        i = 0
+        
+        while i < len(intervals)-1:
+            if intervals[i][1] >= intervals[i+1][0]:
+                if intervals[i][1] <= intervals[i+1][1]:
+                    intervals[i][1] = intervals[i+1][1]       
+                
+                del intervals[i+1]
+            else:
+                i+=1
+        
+        return intervals
+
 class Solution(object):
     def merge(self, intervals):
         intervals = sorted(intervals, key = lambda x: x[0])
