09670d8627ed790752316cd717a40225dfa1def5
Hogyeong Kim <hogilkim@gmail.com>
2022-01-08 12:11:29 -0500

    Jan 8, 2022 322-2

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,3 +1,37 @@
+# second attempt - solved
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount+1]*(amount+1)
+        dp[0] = 0
+        for value in range(amount+1):
+            for coin in coins:
+                if value - coin >= 0:
+                    dp[value] = min(dp[value], dp[value-coin] + 1)
+                
+        return dp[value] if dp[value] < amount+1 else -1
+        
+        
+        
+        
+#         coins = sorted(coins)
+#         dp = [0] * (amount + 1)
+        
+        
+#         for value in range(1, amount+1):
+#             min_coin_nums = 2**31
+#             for i in range(len(coins)-1, -1, -1):
+#                 if value - coins[i] > 0:
+#                     candidate = dp[value-coins[i]]
+#                     if candidate != -1:
+#                         min_coin_nums = min( min_coin_nums, dp[value - coins[i]] + 1 )
+#                 elif value%coins[i] == 0:
+#                     min_coin_nums = min( min_coin_nums, value//coins[i] )
+            
+#             dp[value] = min_coin_nums if min_coin_nums < 2**31 else -1        
+#         return dp[amount]
+
+
+#  -------------------------------------------------------------------------------------
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [amount +1]*(amount+1)
