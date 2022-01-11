9f3f33d9c985c1bbb1a6de9b5fc659bdc4843e03
Hogyeong Kim <hogilkim@gmail.com>
2022-01-10 21:36:08 -0500

    Jan 10, 2022 261-2

--- 261. Graph Valid Tree.py
+++ 261. Graph Valid Tree.py
@@ -1,3 +1,25 @@
+# second attempt - Jan 10 solve again
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        tree_map = {i:[] for i in range(n)}
+        
+        for edge in edges:
+            tree_map[edge[0]].append(edge[1])
+            tree_map[edge[1]].append(edge[0])
+        
+        visited=set()
+        
+        def dfs(node, prev):
+            if node in visited:
+                return False
+            visited.add(node)
+            for next_node in tree_map[node]:
+                if next_node != prev:
+                    if not dfs(next_node, node): return False
+            
+            return True
+        
+        return dfs(0, -1) and len(visited) == n
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         # THIS IS DIFFERENT PROBLEM FROM COURSE SCHEDULE
