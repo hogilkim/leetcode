a32e98ee1a4678018e3c3ff24405e257258e7ad2
Hogyeong Kim <hogilkim@gmail.com>
2022-01-10 21:06:56 -0500

    Jan 10, 2022 435-2

--- 435. Non-overlapping Intervals.py
+++ 435. Non-overlapping Intervals.py
@@ -1,3 +1,23 @@
+# second attempt Jan 10 - solved!
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals = sorted(intervals, key=lambda x:x[1])
+        intervals = sorted(intervals, key=lambda x:x[0])
+        count = 0
+        i=0
+        while i < len(intervals) -1:
+            if intervals[i][0]<=intervals[i+1][0]<intervals[i][1]:
+                # delete i+1
+                if intervals[i][1] <= intervals[i+1][1]:
+                    del intervals[i+1]
+                # delete i
+                else:
+                    del intervals[i]
+                count += 1
+            else:
+                i += 1
+        
+        return count
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         # solve again!
