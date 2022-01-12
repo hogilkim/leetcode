8721d81cc163e9dcde0f29f699b4f55bc74ab149
Hogyeong Kim <hogilkim@gmail.com>
2022-01-11 22:09:50 -0500

    Jan 11, 2022 424-2

--- 424. Longest Repeating Character Replacement.py
+++ 424. Longest Repeating Character Replacement.py
@@ -1,3 +1,21 @@
+# second attempt - Jan 11 2022
+# solve again
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        left = 0
+        curr_len = 0
+        max_len = 0
+        counter = {}
+        for right in range(len(s)):
+            counter[s[right]] = 1 + counter.get(s[right], 0)
+            curr_len += 1
+            if curr_len - max(counter.values()) > k:
+                counter[s[left]] -= 1
+                left += 1
+                curr_len -=1
+            max_len = max(max_len, curr_len)
+        
+        return max_len
 # try to solve again
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
