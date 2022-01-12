955a8b6fe87df5a9ace852d6782af26e659398ca
Hogyeong Kim <hogilkim@gmail.com>
2022-01-11 21:27:54 -0500

    Jan 11, 2022 713

---
+++ 713. Subarray Product Less Than K.py
@@ -0,0 +1,19 @@
+# solve again
+# first attempt - Jan 11
+import collections
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        left = 0 
+        prod = 1
+        count = 0
+        
+        for right in range(len(nums)):
+            prod *= nums[right]
+            while prod >= k and left <= right:
+                prod /= nums[left]
+                left += 1
+            
+            
+            count += right - left + 1
+        
+        return count
