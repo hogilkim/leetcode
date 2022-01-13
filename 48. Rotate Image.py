624a4f7660e46930867297357919dbf41573b1ab
Hogyeong Kim <hogilkim@gmail.com>
2022-01-13 16:26:31 -0500

    Jan 13, 2022 48-2

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -1,3 +1,39 @@
+# solved
+# second attempt - Jan 13, 2022
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        top, bottom = 0, len(matrix)-1
+        left, right = 0, len(matrix[0])-1
+        
+        while left < right or top < bottom:
+            for i in range(right - left):
+                # tl->tr
+                temp_rt = matrix[top+i][right]
+                matrix[top+i][right] = matrix[top][left+i]
+                
+                # rt->rb
+                temp_rb = matrix[bottom][right-i]
+                matrix[bottom][right-i] = temp_rt
+                
+                #rb->lb
+                temp_lb = matrix[bottom-i][left]
+                matrix[bottom-i][left] = temp_rb
+                
+                #lb->tl
+                matrix[top][left+i] = temp_lb
+            
+            left +=1
+            right -=1
+            top+=1
+            bottom-=1
+        
+        return matrix
+    
+
+
 # Solve again
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
