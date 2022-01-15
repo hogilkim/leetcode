3c9444159f321bce5b6705610cf621bc96676ee2
Hogyeong Kim <hogilkim@gmail.com>
2022-01-15 15:19:41 -0500

    Jan 14, 2022 3-2

--- 3 LongestSubstringWithoutRepeatingCharacters.py
+++ 3 LongestSubstringWithoutRepeatingCharacters.py
@@ -1,3 +1,27 @@
+# solved
+# second attempt - Jan 15, 2022
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s: return 0
+        l, r = 0, 1
+        
+        substrings = set([s[0]])
+        max_len = 1
+        
+        while r < len(s):
+            if s[r] not in substrings:
+                substrings.add(s[r])
+            else:
+                while s[l] != s[r]:
+                    substrings.remove(s[l])
+                    l+=1
+                l += 1
+            max_len = max(max_len, r-l+1)
+            r+=1
+            
+            
+        
+        return max_len
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
