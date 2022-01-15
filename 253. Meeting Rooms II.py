e2b2d706e9ff355a89ee10f5fa809fdd18ca432e
Hogyeong Kim <hogilkim@gmail.com>
2022-01-14 22:07:12 -0500

    Jan 14, 2022

--- 253. Meeting Rooms II.py
+++ 253. Meeting Rooms II.py
@@ -1,3 +1,30 @@
+# solve again
+# second attempt - Jan 14, 2022
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        start = []
+        end = []
+        for interval in intervals:
+            start.append(interval[0])
+            end.append(interval[1])
+        
+        start = sorted(start)
+        end = sorted(end)
+        
+        room_counter = 0
+        max_counter = 0
+        start_ptr, end_ptr = 0, 0
+        while start_ptr < len(start):
+            if start[start_ptr] < end[end_ptr]:
+                room_counter += 1
+                start_ptr += 1
+            else:
+                room_counter -= 1
+                end_ptr += 1
+            max_counter = max(max_counter, room_counter)
+            
+        return max_counter
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         start = []
