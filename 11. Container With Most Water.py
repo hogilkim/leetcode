6585b82bba82481faf8236defd8c62406cae086c
Hogyeong Kim <hogilkim@gmail.com>
2022-01-17 14:01:35 -0500

    Jan 17, 2022 11-2

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -1,3 +1,21 @@
+# solved
+# second attempt Jan 17, 2022
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        
+        max_area = 0
+        
+        while left < right:
+            max_area = max(max_area, min(height[left], height[right])*(right - left) )
+            
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        
+        return max_area
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
