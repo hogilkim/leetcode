582d810d0032526450fcf350d858895ed2d33384
Hogyeong Kim <hogilkim@gmail.com>
2022-01-18 11:59:29 -0500

    Jan 18, 2022 98

---
+++ 98. Validate Binary Search Tree.py
@@ -0,0 +1,26 @@
+# solve again
+# First attempt - Jan 18, 2022
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode], floor = float('-inf'), ceiling=float('inf')) -> bool:
+        return not root or (floor < root.val < ceiling and self.isValidBST(root.left, floor, root.val) and self.isValidBST(root.right, root.val, ceiling))
+        
+        
+        
+        
+#         if not root: return True
+        
+#         if root.left:
+#             if root.left.val >= root.val:
+#                 return False
+            
+#         if root.right:
+#             if root.right.val <= root.val:
+#                 return False
+            
+#         return self.isValidBST(root.left) and self.isValidBST(root.right)
