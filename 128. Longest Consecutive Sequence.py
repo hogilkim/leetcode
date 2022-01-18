606c8c2d2ed3c1aa8841a7de9871e6e339691428
Hogyeong Kim <hogilkim@gmail.com>
2022-01-17 22:52:27 -0500

    Jan 17, 2022 128-2

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -1,3 +1,26 @@
+# solve again
+# second attempt - Jan 17, 2022
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        
+        max_len = 0
+        
+        while nums:
+            inc = dec = nums.pop()
+            
+            while inc + 1 in nums:
+                inc += 1
+                nums.remove(inc)
+            while dec - 1 in nums:
+                dec -= 1
+                nums.remove(dec)
+            
+            max_len = max(max_len, inc - dec + 1)
+        
+        return max_len
+
+# O(nlogn) solution
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         
