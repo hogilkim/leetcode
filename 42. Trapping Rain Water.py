db56213512c32a1e5beb667b4008e473863bc0e2
Hogyeong Kim <hogilkim@gmail.com>
2022-01-20 17:37:12 -0500

    Jan 20, 2022 42-2

--- 42. Trapping Rain Water.py
+++ 42. Trapping Rain Water.py
@@ -1,3 +1,28 @@
+# solve again
+# second attempt - Jan 20, 2022
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+
+        
+        l, r = 0, len(height)-1
+        left_max, right_max = height[l], height[r]
+        
+        total_water = 0
+        
+        while l<r:
+            if left_max <= right_max:
+                l+=1
+                left_max = max(left_max, height[l])
+                total_water += left_max - height[l]
+            else:
+                r -= 1
+                right_max = max(right_max, height[r])
+                total_water += right_max - height[r]
+        
+        return total_water
+        
+        
 class Solution:
     def trap(self, height: List[int]) -> int:
         # solve again
