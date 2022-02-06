6270387e04d3588875a44e6862d0241899613364
Hogyeong Kim <hogilkim@gmail.com>
2022-02-05 22:21:03 -0500

    Feb 5, 2022 1650

---
+++ 1650. Lowest Common Ancestor of a Binary Tree III.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        visited = set()
+        
+        while p:
+            visited.add(p.val)
+            p = p.parent
+        
+        while q:
+            if q.val in visited: return q
+            q = q.parent
+        return None
