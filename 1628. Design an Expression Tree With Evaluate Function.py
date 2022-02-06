2246883aaaf921bb5d3d9ae1983a4354af13cdd7
Hogyeong Kim <hogilkim@gmail.com>
2022-02-05 22:58:00 -0500

    Feb 5, 2022 1628

---
+++ 1628. Design an Expression Tree With Evaluate Function.py
@@ -0,0 +1,57 @@
+# solve again
+import abc 
+from abc import ABC, abstractmethod 
+"""
+This is the interface for the expression tree Node.
+You should not remove it, and you can define some classes to implement it.
+"""
+
+class Node(ABC):
+    @abstractmethod
+    # define your fields here
+    def evaluate(self) -> int:
+        pass
+
+class TreeNode(Node):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+    def evaluate(self):
+        if self.val.isdigit():
+            return int(self.val)
+        
+        elif self.val == "+":
+            return self.left.evaluate() + self.right.evaluate()
+        elif self.val == "-":
+            return self.left.evaluate() - self.right.evaluate()
+        elif self.val == "/":
+            return self.left.evaluate() // self.right.evaluate()
+        else:
+            return self.left.evaluate() * self.right.evaluate()
+
+"""    
+This is the TreeBuilder class.
+You can treat it as the driver code that takes the postinfix input
+and returns the expression tree represnting it as a Node.
+"""
+
+class TreeBuilder(object):
+    def buildTree(self, postfix: List[str]) -> 'Node':
+        curr, stack = None, []
+        
+        for char in postfix:
+            curr = TreeNode(char)
+            if not curr.val.isdigit():
+                curr.right = stack.pop()
+                curr.left = stack.pop()
+            stack.append(curr)
+        
+        return curr
+"""
+Your TreeBuilder object will be instantiated and called as such:
+obj = TreeBuilder();
+expTree = obj.buildTree(postfix);
+ans = expTree.evaluate();
+"""
+        
