4e6b1a283bfe8e81f450fbe2108a5fe89ca91990
Hogyeong Kim <hogilkim@gmail.com>
2022-02-18 22:30:21 -0500

    Feb 18, 2022 412

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,5 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        list_of_output = [ 'Fizz' * (not i % 3) + 'Buzz' * (not i % 5 ) or str(i) for i in range(1, n+1) ]
+        
+        return list_of_output
