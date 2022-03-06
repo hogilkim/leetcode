960e31096c7444507ded013ffd240a58905be374
Hogyeong Kim <hogilkim@gmail.com>
2022-03-06 14:14:08 -0500

    Mar 6, 2022 394

---
+++ 394. Decode String.py
@@ -0,0 +1,25 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr_num = 0
+        curr_str = ""
+        
+        for c in s:
+            if c == "[":
+                stack.append(curr_str)
+                stack.append(curr_num)
+                curr_str = ""
+                curr_num = 0
+            
+            elif c == "]":
+                num = stack.pop()
+                prev_str = stack.pop()
+                curr_str = prev_str + curr_str * num
+            
+            elif c.isdigit():
+                curr_num = curr_num*10 + int(c)
+            
+            else:
+                curr_str += c
+        
+        return curr_str
