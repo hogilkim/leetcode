ff0302001f29f6bce9215c53ca2cda5385d90526
Hogyeong Kim <hogilkim@gmail.com>
2022-05-26 20:46:48 -0700

    May 26, 2022 791

---
+++ 791. Custom Sort String.py
@@ -0,0 +1,18 @@
+import collections
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        counter = collections.Counter(s)
+        
+        res = ""
+        
+        for char in order:
+            if char in counter:
+                res += char*counter[char]
+                del counter[char]
+                
+        
+        for key in counter.keys():
+            res+= key*counter[key]
+            
+        
+        return res
