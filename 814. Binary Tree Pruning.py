a96c4e6f7eb5682ff8e070c05f8c1856f1447d37
Hogyeong Kim <hogilkim@gmail.com>
2022-05-31 20:26:45 -0700

    May 31, 2022 814

---
+++ 814. Binary Tree Pruning.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        root.left = self.pruneTree(root.left) if root.left else None
+        root.right = self.pruneTree(root.right) if root.right else None
+        
+        if root.left or root.right or root.val:
+            return root
+        else: return None
+        
