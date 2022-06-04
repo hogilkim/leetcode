5a452a6a3ee178c18c76d528e7f509678772bcc2
Hogyeong Kim <hogilkim@gmail.com>
2022-06-03 22:19:54 -0700

    June 3, 2022 1512

---
+++ 1512. Number of Good Pairs.py
@@ -0,0 +1,11 @@
+import collections
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        dic = collections.defaultdict(int)
+        res = 0
+        
+        for num in nums:
+            res += dic[num]
+            dic[num] += 1
+        
+        return res
