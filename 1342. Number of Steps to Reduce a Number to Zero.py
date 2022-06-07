46e565ff549251e867aa1c3f2de8af718f0b98cf
Hogyeong Kim <hogilkim@gmail.com>
2022-06-06 21:31:32 -0700

    June 6, 2022 1342

---
+++ 1342. Number of Steps to Reduce a Number to Zero.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        
+        step = 0
+        
+        while num:
+            if num%2:
+                num -= 1
+            else:
+                num //= 2
+                
+            
+            step+=1
+        
+        return step
