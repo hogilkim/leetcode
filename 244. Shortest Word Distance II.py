1f0cea77dfcdcff71b759715b7f58f54c91ba67c
Hogyeong Kim <hogilkim@gmail.com>
2022-06-16 21:41:21 -0700

    June 16, 2022 244-2

--- 244. Shortest Word Distance II.py
+++ 244. Shortest Word Distance II.py
@@ -7,22 +7,40 @@ class WordDistance:
             self.word_dict[word].append(idx)
 
     def shortest(self, word1: str, word2: str) -> int:
-        min_heap1, min_heap2 = self.word_dict[word1].copy(),self.word_dict[word2].copy()
-        heapq.heapify(min_heap1)
-        heapq.heapify(min_heap2)
-                
-        curr1, curr2 = heapq.heappop(min_heap1), heapq.heappop(min_heap2)
         
-        min_diff = abs(curr1 - curr2)
-        while min_heap1 or min_heap2:
-            if min_heap1 and (not min_heap2 or curr1 < curr2):
-                curr1 = heapq.heappop(min_heap1)
-            elif min_heap2:
-                curr2 = heapq.heappop(min_heap2)
-            min_diff = min(min_diff, abs(curr1 - curr2))
+        idx_list1, idx_list2 = self.word_dict[word1].copy(), self.word_dict[word2].copy()
         
+        curr1, curr2 = idx_list1.pop(), idx_list2.pop()
+        min_diff = abs(curr1-curr2)
+        while idx_list1 or idx_list2:
+            if idx_list1 and (not idx_list2 or curr1 > curr2):
+                curr1 = idx_list1.pop()
+            else:
+                curr2 = idx_list2.pop()
+            
+            min_diff = min(min_diff, abs(curr1-curr2))
         
         return min_diff
+        
+        
+        
+        
+#         min_heap1, min_heap2 = self.word_dict[word1].copy(),self.word_dict[word2].copy()
+#         heapq.heapify(min_heap1)
+#         heapq.heapify(min_heap2)
+                
+#         curr1, curr2 = heapq.heappop(min_heap1), heapq.heappop(min_heap2)
+        
+#         min_diff = abs(curr1 - curr2)
+#         while min_heap1 or min_heap2:
+#             if min_heap1 and (not min_heap2 or curr1 < curr2):
+#                 curr1 = heapq.heappop(min_heap1)
+#             elif min_heap2:
+#                 curr2 = heapq.heappop(min_heap2)
+#             min_diff = min(min_diff, abs(curr1 - curr2))
+        
+        
+#         return min_diff
 
 
 # Your WordDistance object will be instantiated and called as such:
