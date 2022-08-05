2aa1b823c236889d0daf79f38ee956cb4f148ac7
hogyeongkim <hogilkim@gmail.com>
2022-08-05 10:18:49 -0700

    Aug 5, 2022 377

--- 377. Combination Sum IV.py
+++ 377. Combination Sum IV.py
@@ -1,3 +1,16 @@
+# Third - Aug 5, 2022
+# Solve again
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [0] * (target+1)
+        dp[0]=1
+        for i in range(target+1):
+            for num in nums:
+                if i - num >= 0:
+                    dp[i]+=dp[i-num]
+        
+        return dp[-1]
+        
 # solve again
 # second attempt - Jan 17, 2022
 class Solution:
