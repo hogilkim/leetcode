ddc5e478c1e62b3b270bfef1eaaae226defc38d0
hogyeongkim <hogilkim@gmail.com>
2022-09-06 18:04:06 -0400

    Sep 6, 2022 814-2

--- 814. Binary Tree Pruning.py
+++ 814. Binary Tree Pruning.py
@@ -1,3 +1,15 @@
+class Solution:
+    def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        
+        if not root: return None
+        
+        root.left = self.pruneTree(root.left)
+        root.right = self.pruneTree(root.right)
+        
+        if not root.left and not root.right and not root.val: return None
+        return root
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
