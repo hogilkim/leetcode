2abf09ef35eea9317fcb92c5b1be09ac6d90a72d
hogyeongkim <hogilkim@gmail.com>
2022-10-04 13:05:31 -0400

    Oct 4, 2022 322-3

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -1,3 +1,14 @@
+# Third attempt for TikTok OA - saw answer
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float('inf')] * (amount+1)
+        dp[0] = 0
+        for i in range(1,len(dp)):
+            for coin in coins:
+                if i - coin >= 0:
+                    dp[i] = min(dp[i-coin] + 1, dp[i])
+        return dp[-1] if dp[-1] != float('inf') else -1
+
 # second attempt - solved
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
