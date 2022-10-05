4452d8934c68539d1f4e9451b7e462f2002f5780
hogyeongkim <hogilkim@gmail.com>
2022-10-05 12:23:23 -0400

    Oct 5, 2022 735

--- 735. Asteroid Collision.py
+++ 735. Asteroid Collision.py
@@ -1,3 +1,24 @@
+# second attempt: solved
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = [asteroids[0]]
+        
+        for ast in asteroids[1:]:
+            while stack and stack[-1] > 0 and ast < 0:
+                last = stack.pop()
+                if last > -ast:
+                    ast = 0
+                    stack.append(last)
+                elif last + ast == 0: ast = 0
+                    
+                
+            
+            if ast != 0:
+                stack.append(ast)
+        
+        return stack
+            
+
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
