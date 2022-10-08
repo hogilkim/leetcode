7d7b8d646c4f983204320b0ff3b64496b81a5490
hogyeongkim <hogilkim@gmail.com>
2022-10-07 20:54:52 -0400

    Oct 7, 2022 16-2

--- 16. 3Sum Closest.py
+++ 16. 3Sum Closest.py
@@ -1,3 +1,29 @@
+# second attempt - Oct 7, 2022 
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        diff = float('inf')
+        answer = 0
+        for i in range(len(nums)-2):
+            l, r = i+1, len(nums)-1
+            
+            if i>0 and nums[i] == nums[i-1]: continue
+            
+            while l<r:
+                threeSum = nums[i] + nums[l] + nums[r]
+                
+                if abs(target-threeSum)<diff:
+                    answer = threeSum
+                    diff = abs(target-threeSum)
+
+                if threeSum < target:
+                    l += 1
+                else:
+                    r -= 1
+        
+        return answer
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
