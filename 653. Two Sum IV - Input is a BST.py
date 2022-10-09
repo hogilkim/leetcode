a9d943771d11f80007c2f31427c6edaab7028210
hogyeongkim <hogilkim@gmail.com>
2022-10-09 11:58:42 -0400

    Oct 9, 2022 653

---
+++ 653. Two Sum IV - Input is a BST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+        lookingfor = set()
+        
+        def dfs(node):
+            if not node: return False
+            if node.val in lookingfor:
+                return True
+                        
+            lookingfor.add(k-node.val)
+            
+            return dfs(node.left) or dfs(node.right)
+        
+        
+        return dfs(root)
