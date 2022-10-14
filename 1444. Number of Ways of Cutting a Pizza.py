5b0e61f6e0ae9772f5e462cce16c034bcab7c1b9
hogyeongkim <hogilkim@gmail.com>
2022-10-13 23:01:02 -0400

    Oct 13, 2022 1444-2

--- 1444. Number of Ways of Cutting a Pizza.py
+++ 1444. Number of Ways of Cutting a Pizza.py
@@ -2,6 +2,53 @@ class Solution:
     def ways(self, pizza: List[str], k: int) -> int:
         ROW, COL = len(pizza), len(pizza[0])
         MOD = 10**9+7
+        
+        # making preSum dp
+        preSumdp = [[0]*(COL+1) for _ in range(ROW+1)]
+        
+        for i in range(ROW-1, -1, -1):
+            for j in range(COL-1, -1, -1):
+                preSumdp[i][j] = preSumdp[i][j+1] + preSumdp[i+1][j] - preSumdp[i+1][j+1] + int(pizza[i][j] == "A")
+                
+                
+                #####
+                
+        # memo
+        memo = {}
+        def backtracking(r,c,k):
+            if preSumdp[r][c] == 0: return 0
+            if k == 0: return 1
+            if (r,c,k) in memo: return memo[(r,c,k)]
+                
+                
+            total_cutting = 0
+            
+            # for loop i  r+1 - end:
+                # if preSum[r][c] - preSum[i][c] > 0 
+                    # call backtracking(i, c, k-1)
+            for row in range(r+1, ROW):
+                if preSumdp[r][c] - preSumdp[row][c] > 0:
+                    total_cutting = (total_cutting + backtracking(row, c, k-1))%MOD
+                
+            
+            # for loop j  c+1 - end:
+                # if preSum[r][c] - preSum[r][j] > 0 
+                    # call backtracking(r, j, k-1)
+            for col in range(c+1, COL):
+                if preSumdp[r][c] - preSumdp[r][col] > 0:
+                    total_cutting = (total_cutting + backtracking(r, col, k-1))%MOD
+            
+            memo[(r,c,k)] = total_cutting
+            
+            return total_cutting 
+            
+        
+        return backtracking(0,0,k-1)
+
+class Solution:
+    def ways(self, pizza: List[str], k: int) -> int:
+        ROW, COL = len(pizza), len(pizza[0])
+        MOD = 10**9+7
         preSum = [[0]*(COL+1) for _ in range(ROW+1)]
         
         for row in range(ROW-1, -1, -1):
