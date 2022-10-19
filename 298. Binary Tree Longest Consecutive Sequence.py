92951a30fb2a9fd8915c38874208ae8f4ab46f77
hogyeongkim <hogilkim@gmail.com>
2022-10-19 19:59:13 -0400

    Oct 19, 2022 298-3

--- 298. Binary Tree Longest Consecutive Sequence.py
+++ 298. Binary Tree Longest Consecutive Sequence.py
@@ -4,6 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+class Solution:
+    def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+        
+        
+        def dfs(node, prev_val, sequence):
+            if not node:
+                return sequence
+            
+            curr_sequence = 0
+            
+            if prev_val + 1 == node.val:
+                sequence += 1
+                curr_sequence = sequence
+            else:
+                curr_sequence = 1
+            
+            left = dfs(node.left, node.val, curr_sequence)
+            right = dfs(node.right, node.val, curr_sequence)
+            
+            # return max seqeunce btw sequence, left, right
+            return max(sequence, left, right)
+        
+        return dfs(root, 0, 0)
+        
+        
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 import collections
 class Solution:
     def longestConsecutive(self, root: Optional[TreeNode]) -> int:
