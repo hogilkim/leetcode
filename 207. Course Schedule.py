168bec84004e84242c9c894981f1635c4f1e4a02
hogyeongkim <hogilkim@gmail.com>
2022-10-19 20:26:49 -0400

    Oct 19, 2022 207

--- 207. Course Schedule.py
+++ 207. Course Schedule.py
@@ -1,3 +1,41 @@
+# third - solved
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+            
+        # declare sets visited & path
+        visited = set()
+        path = set()
+        # build dictionary for next_courses
+        next_courses = defaultdict(list)
+        for c1, c2 in prerequisites:
+            next_courses[c2].append(c1)
+            
+            
+        def dfs(key):
+            visited.add(key)
+            path.add(key)
+            # loop through dic[key]:
+            for next_c in next_courses[key]:
+                # if in path: return False
+                if next_c in path: return False
+                # not in visited, calll another dfs
+                elif next_c not in visited:
+                    if not dfs(next_c): return False
+            
+            path.remove(key)
+            
+            return True
+        
+        # for loop from 0 to n-1
+        for c in range(numCourses):
+            # call dfs
+            if c not in visited:
+                if not dfs(c): return False
+        
+        return True
+        
+        
 # second attempt - solved!
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
