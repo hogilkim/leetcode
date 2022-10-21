dd3dbefd3e9bf6047c7f229fea576de551ce1b76
hogyeongkim <hogilkim@gmail.com>
2022-10-21 11:46:56 -0400

    Oct 21, 2022 1099-2

--- 1099. Two Sum Less Than K.py
+++ 1099. Two Sum Less Than K.py
@@ -1,5 +1,34 @@
 class Solution:
     def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        
+        # not sorted, k can be any random number
+        
+        nums = sorted(nums)
+        
+        # two ptrs l r
+        l, r = 0, len(nums)-1
+        
+        max_twosum = 0
+        
+        # while l<r:
+        while l<r:
+            # if l+r < k:
+            if nums[l] + nums[r] < k:
+                
+                # save max result
+                max_twosum = max(max_twosum, nums[l] + nums[r])
+                l+= 1
+            
+            else:
+                r -= 1
+            
+        
+        return max_twosum if max_twosum>0 else -1
+        
+        
+
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
         l, r = 0, len(nums)-1
         res = -1
         nums = sorted(nums)
