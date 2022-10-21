a80ee43db19ef0f4d45a0fc7be83dc9a3e235d74
hogyeongkim <hogilkim@gmail.com>
2022-10-21 13:31:15 -0400

    Oct 20, 2022 947-2

--- 947. Most Stones Removed with Same Row or Column.py
+++ 947. Most Stones Removed with Same Row or Column.py
@@ -1,6 +1,56 @@
 from collections import defaultdict
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
+        
+        row_map, col_map = defaultdict(list), defaultdict(list)
+        stones_set = set()
+        
+        for r,c in stones:
+            row_map[r].append((r,c))
+            col_map[c].append((r,c))
+            stones_set.add((r,c))
+            
+        
+        # def dfs(r,c):
+        def dfs(r,c):
+            # remove (r,c) in stones_set
+            stones_set.remove((r,c))
+            
+            # for loop through row_map[r]
+            for nr, nc in row_map[r]:
+                # if next r,c in stones_set:
+                if (nr,nc) in stones_set:
+                    dfs(nr,nc)
+                    # dfs()
+                    
+            # for loop through col_map[c]
+            for nr, nc in col_map[c]:
+                # if next r,c in stones_set:
+                if (nr,nc) in stones_set:
+                    # dfs
+                    dfs(nr,nc)
+                    
+                    
+        groups = 0
+
+        
+        
+        # for loop
+        for r,c in stones:
+            # dfs()
+            if (r,c) in stones_set:
+            # groups += 1
+                dfs(r,c)
+                groups+=1
+        
+        return len(stones) - groups
+    
+    
+    # Time Complextiy: O(n) Space: O(n)
+
+from collections import defaultdict
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
 
         # make hash map that shares same row and column
         row_map, col_map = defaultdict(list), defaultdict(list)
