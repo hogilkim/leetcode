3f5a3c9957ec0842df3483af4b832ef87ecafe20
hogyeongkim <hogilkim@gmail.com>
2022-10-22 18:10:49 -0400

    Oct 22, 2022 694-2

--- 694. Number of Distinct Islands.py
+++ 694. Number of Distinct Islands.py
@@ -1,3 +1,39 @@
+# second attempt - Oct 22, 2022 solved
+
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        shapes = set()
+        visited = set()
+        
+        dic = {
+            (1,0): "d",
+            (0,1): "r",
+            (-1,0): "u",
+            (0,-1): "l"
+        }
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
+        
+        ROW, COL = len(grid), len(grid[0])
+        
+        def dfs(r,c, prev_dir):
+            
+            shape = prev_dir
+            visited.add((r,c))
+            
+            for rd, cd in directions:
+                nr, nc = r+rd, c+cd
+                if 0<=nr<ROW and 0<=nc<COL and grid[nr][nc] == 1 and (nr,nc) not in visited:
+                    shape += dfs(nr,nc, dic[(rd,cd)])
+            
+            return shape +"b"
+        
+        for row in range(ROW):
+            for col in range(COL):
+                if (row,col) not in visited and grid[row][col] == 1:
+                    shapes.add(dfs(row,col,""))
+        
+        return len(shapes)
+
 class Solution:
     def numDistinctIslands(self, grid: List[List[int]]) -> int:
         shapes = set()
