7e5e66ff91c31174b13c2b1c799554889c90c0d5
hogyeongkim <hogilkim@gmail.com>
2022-10-29 15:41:48 -0400

    Oct 29, 2022 772-2

--- 772. Basic Calculator III.py
+++ 772. Basic Calculator III.py
@@ -1,5 +1,74 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        
+        def helper(num, operator):
+            if operator == "+":
+                # append num stack
+                stack.append(num)
+            # if -
+            elif operator == "-":
+                # append -num2 stack
+                stack.append(-num)
+            # if * 
+                # append stack[-1]*num
+            elif operator == "*":
+                stack.append(stack.pop()*num)
+                
+            # if /
+            elif operator == "/":
+                stack.append(int(stack.pop()/num))
+                # append stack[-1]/num
+            
+
+        
+        stack =[]
+        
+        prev_op = "+"
+        num = 0
+        ops = list("+-/*")
+        
+        for char in s+"+":
+            # if digit
+            if char.isdigit():
+                num = 10*num + int(char)
+            
+            elif char in ops:
+                # call helper func (prev_op, num)
+                helper(num, prev_op)
+                
+                prev_op = char
+                num = 0
+            
+            # if char (
+            elif char == "(":
+                # append num
+                # stack.append(num)
+                num = 0
+                # append operator & prev_op = "+"
+                stack.append(prev_op)
+                prev_op = "+"
+                
+            # if char )
+            elif char == ")":
+                helper(num, prev_op)
+                num = 0
+                prev_op = ")"
+                
+                temp =0
+                while stack[-1] not in ops:
+                    temp += stack.pop()
+                helper(temp, stack.pop())
+    
+        return sum(stack)
+                
+                
+                
+            
+            
+
+
+class Solution:
+    def calculate(self, s: str) -> int:
         def update(op, num):
             if op == "+": stack.append(num)
             elif op == "-": stack.append(-num)
