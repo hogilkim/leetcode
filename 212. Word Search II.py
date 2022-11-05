d703c8526e53b562758ca0495cacceb84b679e79
hogyeongkim <hogilkim@gmail.com>
2022-11-05 13:45:24 -0400

    Nov 5, 2022 212-2

--- 212. Word Search II.py
+++ 212. Word Search II.py
@@ -1,5 +1,51 @@
 class TrieNode:
     def __init__(self):
+        self.word = None
+        self.children = {}
+        
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trieroot = TrieNode()
+        
+        for word in words:
+            node = trieroot
+            
+            for idx, char in enumerate(word):
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+                if idx == len(word)-1:
+                    node.word = word
+        
+        directions = [(1,0), (0,1), (-1,0), (0,-1)]
+        
+        ROW, COL = len(board), len(board[0])
+        path = set()
+        
+        res = []
+        
+        def dfs(row, col, trienode):
+            if trienode.word:
+                res.append(trienode.word)
+                trienode.word = None
+            path.add((row,col))
+            
+            for rd, cd in directions:
+                nr, nc = row+rd, col+cd
+                if 0<=nr<ROW and 0<=nc<COL and (nr,nc) not in path and board[nr][nc] in trienode.children:
+                    dfs(nr,nc,trienode.children[board[nr][nc]])
+            
+            path.remove((row,col))
+        
+        for r in range(ROW):
+            for c in range(COL):
+                if board[r][c] in trieroot.children:
+                    node = trieroot
+                    dfs(r,c,node.children[board[r][c]])
+        return res
+
+class TrieNode:
+    def __init__(self):
         self.children = {}
         self.isWord = False
     def addWord(self, word):
