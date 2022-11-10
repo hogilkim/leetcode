af3fd8a57180b63b98e49effcadba6e70a3ba623
hogyeongkim <hogilkim@gmail.com>
2022-11-09 20:59:44 -0500

    Nov 9, 2022 254

--- 254. Factor Combinations.py
+++ 254. Factor Combinations.py
@@ -1,3 +1,22 @@
+# second try Nov 9 Not prefect
+
+import math
+class Solution:
+    def getFactors(self, n: int) -> List[List[int]]:
+        res = []
+        
+        def dfs(path, divstart, target):
+            if len(path)>0: res.append(path+[target])
+            
+            
+            for div in range(divstart, int(math.sqrt(target)+1)):
+                if target%div == 0:
+                    dfs(path+[div], div, target//div)
+        
+        
+        dfs([],2,n)
+        return res
+
 import math
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
