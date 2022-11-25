e4c980dcf5d014364149f9ec6d4072fa7ea21afb
hogyeongkim <hogilkim@gmail.com>
2022-11-24 20:10:51 -0500

    Nov 24, 2022 907

--- 907. Sum of Subarray Minimums.py
+++ 907. Sum of Subarray Minimums.py
@@ -1,3 +1,16 @@
+class Solution:
+    def sumSubarrayMins(self, arr: List[int]) -> int:
+        MOD = 10**9+7
+        res = 0
+        stack = []
+        arr = [0] + arr + [0]
+        for idx, num in enumerate(arr):
+            while stack and arr[stack[-1]] > num:
+                curr = stack.pop()
+                res += arr[curr]*(idx-curr)*(curr-stack[-1])
+            stack.append(idx)
+        
+        return res%MOD
 # solve again
 # very hard for me. Took too long time to just understand.
 class Solution:
