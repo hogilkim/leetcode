cd26b9513ee79367f1395c702951d7333e3b1894
hogyeongkim <hogilkim@gmail.com>
2022-11-26 22:32:54 -0500

    Nov 26, 2022 1235-2

--- 1235. Maximum Profit in Job Scheduling.py
+++ 1235. Maximum Profit in Job Scheduling.py
@@ -1,6 +1,20 @@
 import bisect
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = sorted(zip(startTime,endTime, profit), key=lambda x:x[1])
+        
+        dp = [[0,0]]
+        
+        for s,e,p in jobs:
+            i = bisect.bisect_left(dp, [s+1])-1
+            if dp[i][1] + p > dp[-1][1]:
+                dp.append([e, dp[i][1]+p])
+        
+        return dp[-1][1]
+
+import bisect
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         n = len(startTime)
         start, end, profit = zip(*sorted(zip(startTime, endTime, profit)))
         
