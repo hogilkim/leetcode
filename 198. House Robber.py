571f7d1132bd7c5a2b60dc42c3071e25fb726cd3
hogyeongkim <hogilkim@gmail.com>
2022-12-14 10:32:05 -0500

    Dec 14, 2022 198

--- 198. House Robber.py
+++ 198. House Robber.py
@@ -1,3 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        nums = [0,0] + nums
+        dp = [0]*len(nums)
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+        
+        return dp[-1]
+
 # second try - solved! but previous one better
 
 class Solution:
