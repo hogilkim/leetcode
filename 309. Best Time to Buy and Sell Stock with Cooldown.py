81dd5a43509b756c0a47a040718af6c05a138bd0
hogyeongkim <hogilkim@gmail.com>
2022-12-23 23:57:05 -0500

    Dec 23, 2022 309-2

--- 309. Best Time to Buy and Sell Stock with Cooldown.py
+++ 309. Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,3 +1,28 @@
+# second try, solve again
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+
+        def dfs(i, buying):
+            if i >= len(prices): return 0
+            elif (i, buying) in dp: return dp[(i, buying)]
+
+            cooldown = dfs(i+1, buying)
+
+            if buying:
+                buy = dfs(i+1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i+2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+
+        return dfs(0, True)
+
+
+
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
     
