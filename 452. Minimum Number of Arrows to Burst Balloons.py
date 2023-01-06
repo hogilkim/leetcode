e7c09ccc9d150d4a9f0236846d89669e88fde206
hogyeongkim <hogilkim@gmail.com>
2023-01-05 23:16:52 -0500

    Jan 5, 2022 452-2

--- 452. Minimum Number of Arrows to Burst Balloons.py
+++ 452. Minimum Number of Arrows to Burst Balloons.py
@@ -1,6 +1,19 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points = sorted(points, key=lambda x:x[0])
+        overlaps = [ [points[0][0], points[0][1]] ]
+
+        for beg, end in points[1:]:
+            if overlaps[-1][0] <= beg <= overlaps[-1][1]:
+                overlaps[-1][0] = max(beg, overlaps[-1][0])
+                overlaps[-1][1] = min(end, overlaps[-1][1])
+            else:
+                overlaps.append([beg,end])
+        return len(overlaps)
+
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points = sorted(points, key=lambda x:x[0])
         
         overlaps = [points[0]]
         
