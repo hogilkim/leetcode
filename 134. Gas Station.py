481af5c19781f480bfb24defd02b7de3fa4c72e2
hogyeongkim <hogilkim@gmail.com>
2023-01-07 15:53:15 -0500

    Jan 7, 2022 134-2

--- 134. Gas Station.py
+++ 134. Gas Station.py
@@ -1,5 +1,22 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        diff = [gas[i]-cost[i] for i in range(len(gas))]
+
+        if sum(diff)<0: return -1
+
+        partsum = 0
+        start = 0
+
+        for i in range(len(diff)):
+            partsum += diff[i]
+            if partsum <0:
+                start = i + 1
+                partsum = 0
+        
+        return start
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         diff = [gas[i] - cost[i] for i in range(len(gas))]
         
         
