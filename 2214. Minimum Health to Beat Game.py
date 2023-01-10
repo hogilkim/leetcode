bbb379ea1f98bf829955a375fc8a40cb9cebf1d8
hogyeongkim <hogilkim@gmail.com>
2023-01-09 19:26:41 -0500

    Jan 9, 2022 2214-2

--- 2214. Minimum Health to Beat Game.py
+++ 2214. Minimum Health to Beat Game.py
@@ -1,5 +1,15 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
+        # res = 0
+
+        damage = sorted(damage)
+
+        damage[-1] = max(damage[-1]-armor, 0)
+
+        return sum(damage)+1
+
+class Solution:
+    def minimumHealth(self, damage: List[int], armor: int) -> int:
         dmgsum = sum(damage)
         res = dmgsum
         
