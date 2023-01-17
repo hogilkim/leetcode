acbe88d69410070866aa79d757e80f3a31340749
hogyeongkim <hogilkim@gmail.com>
2023-01-16 23:07:21 -0500

    Jan 16, 2023 926-3

--- 926. Flip String to Monotone Increasing.py
+++ 926. Flip String to Monotone Increasing.py
@@ -1,3 +1,17 @@
+class Solution:
+    def minFlipsMonoIncr(self, s: str) -> int:
+        count0 = s.count('0')
+        count1 = 0
+        res = min(count0, len(s)-count0)
+        for char in s:
+            if char == '0':
+                count0 -= 1
+            else:
+                count1 += 1
+                res = min(count0 + count1 - 1, res)
+        
+        return res
+
 # solve again
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
