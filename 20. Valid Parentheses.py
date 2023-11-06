f7e28f01aa5e1869a2a6a931976cadf1afd386ea
hogyeongkim <hogilkim@gmail.com>
2023-11-05 22:18:38 -0800

    Nov 5, 2023 20-2

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,6 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        converter = {']':'[', '}': '{', ')': '('}
+        for char in s:
+            if char in "({[": stack.append(char)
+            elif not stack: return False
+            elif stack and stack[-1] == converter[char]: stack.pop()
+            else: return False
+        
+        return len(stack) == 0
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
         for char in s:
             if char == '(':
                 stack.append(char)
