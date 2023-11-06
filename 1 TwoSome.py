346d0a30af4a9e794b67187c008ca105873a0227
hogyeongkim <hogilkim@gmail.com>
2023-11-05 21:31:50 -0800

    Nov 5, 2023 1-2

--- 1 TwoSome.py
+++ 1 TwoSome.py
@@ -1,3 +1,13 @@
+# Nov 5, 2023 
+from collections import defaultdict
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = defaultdict()
+
+        for idx, num in enumerate(nums):
+            if target-num in dic: return [dic[target-num], idx]
+            else: dic[num]=idx
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # solved twice, success
