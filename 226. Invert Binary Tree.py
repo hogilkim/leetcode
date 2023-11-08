95bf54c3c4cddfcd4f4611369f832b5fcf29e5cc
hogyeongkim <hogilkim@gmail.com>
2023-11-07 23:06:24 -0800

    Nov 7, 2023 226-3

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -1,3 +1,12 @@
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        if not root: return None
+        
+        root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
+
+        return root
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
