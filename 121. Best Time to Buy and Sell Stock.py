2badb6b8c58ddad7054fbbcf0f991b8f04ab0efa
hogyeongkim <hogilkim@gmail.com>
2023-11-07 21:59:18 -0800

    Nov 7, 2023 121-2

--- 121. Best Time to Buy and Sell Stock.py
+++ 121. Best Time to Buy and Sell Stock.py
@@ -1,6 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = float('inf')
+        max_profit = 0
+
+        for num in prices:
+            min_price = min(min_price, num)
+            max_profit = max(max_profit, num-min_price)
+        
+        return max_profit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
 
         max_profit = 0
 
