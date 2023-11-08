d2a34cd2d155023c6723c401fe4e77923549d85d
hogyeongkim <hogilkim@gmail.com>
2023-11-07 22:15:15 -0800

    Nov 7, 2023 125-2

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,5 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+
+        for char in s:
+            if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+                new_s += char.lower()
+            elif '0' <= char <= '9':
+                new_s += char
+        
+        return new_s == new_s[::-1]
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
         lower_s = ""
         for ch in s:
             if ch.isalnum():
