d8ae2b0a7a0f8b83b1ecf6a580b02f51283a5ceb
hogyeongkim <hogilkim@gmail.com>
2023-11-08 13:53:08 -0800

    Nov 8, 2023 242-2

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,3 +1,22 @@
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        LEN = len(s)
+        counter = collections.Counter()
+
+        for i in range(LEN):
+            counter[s[i]] += 1
+            counter[t[i]] -= 1
+        
+        for char in s:
+            if counter[char] != 0: return False
+        
+        return True
+
+        #return collections.Counter(s) == collections.Counter(t)
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
