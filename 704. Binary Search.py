a7f44988188dfdf754d1b5ddbda9442de37e17c3
hogyeongkim <hogilkim@gmail.com>
2023-11-08 14:04:22 -0800

    Nov 8, 2023 704-2

--- 704. Binary Search.py
+++ 704. Binary Search.py
@@ -1,5 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        while l < r:
+            mid = (l+r)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid
+        
+        return r if nums[r] == target else -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums)-1
         mid = (left+right)//2
         while left <= right:
