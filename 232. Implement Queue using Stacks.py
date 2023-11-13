31bec2493529f651a5e7a0ad0bfc2dd41a65b095
hogyeongkim <hogilkim@gmail.com>
2023-11-12 21:13:33 -0800

    Nov 12, 2023 232-2

--- 232. Implement Queue using Stacks.py
+++ 232. Implement Queue using Stacks.py
@@ -1,3 +1,30 @@
+# Nov 12, 2023 232-3
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        if not self.stack2:
+            self.move()
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        if not self.stack2:
+            self.move()
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        return not self.stack1 and not self.stack2
+    def move(self) -> None:
+        while self.stack1:
+            self.stack2.append(self.stack1.pop())
+
 #2 
 class MyQueue:
 
