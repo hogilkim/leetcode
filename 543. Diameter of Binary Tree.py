946a9acaf7e5fbbbe2551c5536cc29c4d1a3360a
hogyeongkim <hogilkim@gmail.com>
2023-11-16 22:05:47 -0800

    Nov 16, 2023 543-2

--- 543. Diameter of Binary Tree.py
+++ 543. Diameter of Binary Tree.py
@@ -1,3 +1,24 @@
+# Nov 16, 2023 
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxlen = 0
+
+        def dfs(node):
+            if not node: return 0
+
+            nonlocal maxlen
+            
+            left = dfs(node.left)
+            right = dfs(node.right)
+
+            maxlen = max(maxlen, left + right)
+
+            return max(left, right) + 1
+        
+        dfs(root)
+        return maxlen
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
