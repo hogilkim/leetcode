87d9bce10e1f4e84d45a50e17e66b75d624fbacb
hogyeongkim <hogilkim@gmail.com>
2023-11-17 23:07:01 -0800

    Nov 17, 2023 53-3

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -1,3 +1,17 @@
+# Nov 17, 2023 53-3
+# solve again
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        currsum = 0
+        maxsum = float('-inf')
+
+        for num in nums:
+            currsum += num
+            maxsum = max(maxsum, currsum)
+            if currsum < 0: currsum = 0
+        
+        return maxsum
+
 # Second Attempt, looked at the answer.
 
 class Solution:
