1fdadbfd9b901b42add2368ed110fd7271acdb6e
hogyeongk <hogyeongk@juniper.net>
2023-11-25 04:50:01 -0800

    Nov 25, 2023 133-3

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -1,3 +1,23 @@
+# Nov 25, 2023 133-3 solve again
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        og_to_clone = {}
+
+        def dfs(n):
+            if n in og_to_clone:
+                return og_to_clone[n]
+
+            copy = Node(n.val)
+            og_to_clone[n] = copy
+            for nei in n.neighbors:
+                copy.neighbors.append(dfs(nei))
+            
+            return copy
+        
+        return dfs(node) if node else None
+
 # second try, couldn't solve
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
