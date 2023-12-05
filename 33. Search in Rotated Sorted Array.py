d78bb455b83b67fbf8bc1eb51582a301280a93f8
hogilkim <hogilkim@gmail.com>
2023-12-05 04:31:10 -0800

    Dec 5, 2023 33-4

--- 33. Search in Rotated Sorted Array.py
+++ 33. Search in Rotated Sorted Array.py
@@ -1,3 +1,27 @@
+# Fourth attempt - solve again 
+# Dec 5, 2023 33-4
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+            print(l, mid, r)
+
+            if nums[mid] == target: return mid
+
+            if nums[l] <= nums[mid]:
+                if nums[l] <= target < nums[mid]: r = mid - 1
+                else: l = mid + 1
+            else: 
+                if nums[mid] < target <= nums[r]: l = mid + 1
+                else: r = mid - 1
+        
+        return -1
+                
+            
+
 # Third time. solve again.
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
