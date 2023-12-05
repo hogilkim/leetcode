d6c8702ad53f651c8928d1fa79b2579c4c826708
hogilkim <hogilkim@gmail.com>
2023-12-04 17:00:35 -0800

    Dec 5, 2023 944-2

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -1,3 +1,40 @@
+# Dec 5, 2023 944-2 solved
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROW, COL = len(grid), len(grid[0])
+        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+        queue = deque([])
+        visited = set()
+        orange_count = 0
+        
+        for row in range(ROW):
+            for col in range(COL):
+                if grid[row][col] == 2: 
+                    queue.append((row, col))
+                
+                elif grid[row][col] == 1:
+                    orange_count += 1
+
+        if not orange_count: return 0
+        orange_count += len(queue)
+
+        
+        minute = -1
+        while queue:
+            minute += 1
+            for _ in range(len(queue)):
+                orange_count -= 1
+                row, col = queue.popleft()
+                
+                for rdir, cdir in dirs:
+                    nr, nc = row + rdir, col + cdir
+                    if 0<=nr<ROW and 0<=nc<COL and (nr, nc) not in visited and grid[nr][nc]==1:
+                        visited.add((nr, nc))
+                        queue.append((nr,nc))
+        print(orange_count)
+        return minute if orange_count == 0 else -1
+
 import collections
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
