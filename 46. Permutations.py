e1e6639acaef445b978a0b27cecc16cbd556e84f
hogilkim <hogilkim@gmail.com>
2023-12-05 18:13:08 -0800

    Dec 6, 2023 46-2

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -1,3 +1,26 @@
+# Dec 6, 2023 46-2
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        path = set()
+        res = []
+
+        def backtracking(curr_list):
+            if len(curr_list) == len(nums):
+                res.append(curr_list.copy())
+                return
+            
+            for i in range(len(nums)):
+                if i not in path:
+                    path.add(i)
+                    curr_list.append(nums[i])
+                    backtracking(curr_list.copy())
+                    path.remove(i)
+                    curr_list.pop()
+        
+        backtracking([])
+
+        return res
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
