df339bc1b800ee0343f8faf48b6deb8c15dd28da
hogilkim <hogilkim@gmail.com>
2023-12-05 18:07:21 -0800

    Dec 6, 2023 39-2

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -1,3 +1,27 @@
+# Dec 6, 2023 39-2
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def backtrack(idx, curr_list):
+            if idx == len(candidates): return
+
+            if sum(curr_list) == target: 
+                res.append(curr_list.copy())
+                return
+            elif sum(curr_list) > target: return
+
+            # include curr idx
+            curr_list.append(candidates[idx])
+            backtrack(idx, curr_list.copy())
+            # X include curr idx
+            curr_list.pop()
+            backtrack(idx+1, curr_list.copy())
+
+        if candidates: backtrack(0, [])
+        
+        return res
+
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
