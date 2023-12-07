f1c60b0175d109a47dcf06601578a240d213a6f6
hogilkim <hogilkim@gmail.com>
2023-12-07 06:50:55 -0800

    Dec 7, 2023 75-2

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -1,3 +1,23 @@
+# Dec 7, 2023 75-2 solve again
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l, mid, r = 0, 0, len(nums)-1
+
+        while mid <= r:
+            print(l,mid,r,nums)
+            if nums[mid] == 0:
+                nums[l], nums[mid] = nums[mid], nums[l]
+                mid += 1
+                l += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:
+                nums[mid], nums[r] = nums[r], nums[mid]
+                r -= 1
+
 # solve again
 # first attempt - Jan 15, 2022
 import collections
