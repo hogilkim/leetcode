acf923901ff3cc2412581e49ff11f3c00d83b032
hogyeongkim <hogilkim@gmail.com>
2023-12-13 23:02:35 -0800

    Dec 13, 2023 5-4

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -1,3 +1,20 @@
+# solved, but maybe solve again
+# Dec 13, 2023 5-4
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            res = max(self.helper(s, i, i), self.helper(s,i,i+1), res, key=len)
+        
+        return res
+        
+    def helper(self, s, l ,r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        return s[l+1:r]
+        
+
 # Try this again
 class Solution:
     
