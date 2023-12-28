96c067ec93a730c40f1710daf06984560e182471
hogyeongkim <hogilkim@gmail.com>
2023-12-28 15:35:32 -0800

    Dec 28, 2023 314-2

--- 314. Binary Tree Vertical Order Traversal.py
+++ 314. Binary Tree Vertical Order Traversal.py
@@ -1,9 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Dec 28, 2023 314-2
+from collections import deque, defaultdict
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        vdict = defaultdict(list)
+
+        queue = deque([(root, 0)]) if root else deque([])
+
+        while queue:
+            node, pos = queue.popleft()
+            vdict[pos].append(node.val)
+            if node.left:
+                queue.append((node.left, pos-1))
+            if node.right:
+                queue.append((node.right, pos+1))
+        
+        return [vdict[key] for key in sorted(vdict.keys())]
+
 import collections
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
