834c02933ca73ee9ddb7fe21aa85d6f079020943
hogyeongkim <hogilkim@gmail.com>
2023-12-30 17:29:49 -0800

    Dec 30, 2023 799-2

--- 799. Champagne Tower.py
+++ 799. Champagne Tower.py
@@ -1,3 +1,25 @@
+# Solve again
+# Dec 30, 2023 779-2 
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        curr = [poured]
+
+        for row in range(query_row + 1):
+            nxt = [0]*(row+2)
+            cups = row+1
+
+            for col in range(cups):
+                if curr[col]>=1:
+                    nxt[col] += (curr[col]-1)/2
+                    nxt[col+1] += (curr[col]-1)/2
+                    curr[col] = 1
+                
+            if row != query_row: curr = nxt
+        
+
+        return curr[query_glass]
+
+
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         curr = [poured]
