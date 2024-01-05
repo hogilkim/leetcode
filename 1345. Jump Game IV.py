33dbe40e4bc170f35f97eb9b60c8de8875cddf48
hogyeongkim <hogilkim@gmail.com>
2024-01-04 22:13:29 -0800

    Jan 4, 2024 1345-2

--- 1345. Jump Game IV.py
+++ 1345. Jump Game IV.py
@@ -1,3 +1,41 @@
+# almost solved. Needed to remove duplicate process
+# Jan 4, 2024 1345-2
+from collections import defaultdict, deque
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        num_to_idx = defaultdict(list)
+        for idx, num in enumerate(arr):
+            num_to_idx[num].append(idx)
+        queue = deque([(0,0)])
+        visited = set()
+        visited.add(0)
+
+
+        while queue:
+            
+            idx, steps = queue.popleft()
+            if idx == len(arr)-1: return steps
+
+            # append idx with same num
+            curr_num = arr[idx]
+            if curr_num in num_to_idx:
+                for i in range(len(num_to_idx[curr_num])-1,-1,-1):
+                    new_idx = num_to_idx[curr_num][i]
+                    if new_idx not in visited:
+                        queue.append((new_idx, steps+1))
+                        visited.add(new_idx)
+                del num_to_idx[curr_num]
+            
+            if idx-1 not in visited and idx-1 > 0:
+                queue.append((idx-1, steps+1))
+                visited.add(idx-1)
+            
+            if idx+1 not in visited and idx + 1 < len(arr):
+                queue.append((idx+1, steps+1))
+                visited.add(idx+1)
+        
+
+
 # solve again
 import collections
 class Solution:
