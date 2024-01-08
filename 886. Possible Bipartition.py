e6dde288b41db371cf46cccab164fca448c16cc4
hogyeongkim <hogilkim@gmail.com>
2024-01-08 11:11:29 -0800

    Jan 8, 2024 886-3

--- 886. Possible Bipartition.py
+++ 886. Possible Bipartition.py
@@ -1,3 +1,32 @@
+# solve again, Jan 8, 2024 886-3
+from collections import defaultdict
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        if n == 1 or not dislikes: return True
+        which_team = [0] * (n+1)
+        dislike_map = defaultdict(list)
+        BLUE = 1; RED = -1; NO_TEAM = 0
+        
+        for p1, p2 in dislikes:
+            dislike_map[p1].append(p2)
+            dislike_map[p2].append(p1)
+        
+        def dfs(p, color):
+            which_team[p] = color
+
+            for dislikep in dislike_map[p]:
+                if which_team[dislikep] == color: return False
+                if which_team[dislikep] == NO_TEAM and not dfs(dislikep, -1*color):
+                    return False
+            
+            return True
+        
+
+        for p in range(1, n+1):
+            if which_team[p] == NO_TEAM:
+                if not dfs(p, BLUE): return False
+        return True
+
 # solve again
 from collections import defaultdict
 class Solution:
