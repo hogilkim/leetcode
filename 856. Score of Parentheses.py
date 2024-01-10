d9a1f328d24a44760c98d3cb747b60c830421f10
hogyeongkim <hogilkim@gmail.com>
2024-01-09 22:28:33 -0800

    Jan 9, 2024 856-2

--- 856. Score of Parentheses.py
+++ 856. Score of Parentheses.py
@@ -1,3 +1,18 @@
+# Solve again, Jan 9, 2024 856-2
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        scores = [0]
+
+        for char in s:
+            if char == "(":
+                scores.append(0)
+            else:
+                val = scores.pop()
+                scores[-1] += max(val*2, 1)
+        
+        return scores[0]
+
+
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         stack = [0]
