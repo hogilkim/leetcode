d4d55699ae3b36c88b734573a57b617c44a8468b
hogyeongkim <hogilkim@gmail.com>
2024-01-21 12:54:58 -0800

    Jan 21, 2024 465-2

--- 465. Optimal Account Balancing.py
+++ 465. Optimal Account Balancing.py
@@ -1,3 +1,34 @@
+# solve again Jan 21, 2024 465-2
+from collections import defaultdict
+class Solution:
+    def minTransfers(self, transactions: List[List[int]]) -> int:
+        trans = defaultdict(int)
+        for sender, receiver, amount in transactions:
+            trans[sender] -= amount
+            trans[receiver] += amount
+
+        balance = [value for value in trans.values() if value != 0]
+        
+        def backtracking(idx, balance):
+            if idx == len(balance): return 0
+            if balance[idx] == 0: return backtracking(idx+1, balance.copy())
+
+            mintrans = float('inf')
+            
+            for i in range(idx+1, len(balance)):
+                # multiply should be negative
+                if balance[i] * balance[idx] < 0:
+                    # adjust balance
+                    balance[i] += balance[idx]
+                    # call backtracking, which return minimum transactions
+                    mintrans = min(mintrans, backtracking(idx+1, balance.copy()) + 1 )
+                    # revert balance
+                    balance[i] -= balance[idx]
+            
+            return mintrans
+
+        return backtracking(0, balance.copy())
+
 from collections import defaultdict
 class Solution:
     def minTransfers(self, transactions: List[List[int]]) -> int:
