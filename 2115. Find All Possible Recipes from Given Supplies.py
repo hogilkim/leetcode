b99fd8a272160d51e67a35c403e52a89504b407d
hogyeongkim <hogilkim@gmail.com>
2024-01-29 09:15:57 -0800

    Jan 29, 2024 2115-2

--- 2115. Find All Possible Recipes from Given Supplies.py
+++ 2115. Find All Possible Recipes from Given Supplies.py
@@ -1,3 +1,31 @@
+# Solve again, Jan 29, 2024 2115-2
+from collections import defaultdict, deque
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        
+        ingred_to_food = defaultdict(list)
+        food_ing_num = defaultdict(int)
+
+        for foodname, items in zip(recipes, ingredients):
+            food_ing_num[foodname] = len(items)
+            for item in items:
+                ingred_to_food[item].append(foodname)
+        
+        queue = deque(supplies)
+        res = []
+
+        while queue:
+            item = queue.popleft()
+
+            for food in ingred_to_food[item]:
+                food_ing_num[food] -= 1
+                if food_ing_num[food] == 0:
+                    res.append(food)
+                    queue.append(food)
+            
+        return res
+
+
 from collections import defaultdict, deque
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
