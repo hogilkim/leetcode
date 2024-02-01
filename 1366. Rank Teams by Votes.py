6edae66f11fe1421613ae52a463e60a094ec3e70
hogyeongkim <hogilkim@gmail.com>
2024-02-01 08:46:42 -0800

    Feb 1, 2024 1366-2

--- 1366. Rank Teams by Votes.py
+++ 1366. Rank Teams by Votes.py
@@ -1,3 +1,17 @@
+# Feb 1, 2024 1366-2
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        ranks = {char:[0]*len(votes[0]) for char in votes[0]}
+
+        for vote in votes:
+            for idx, char in enumerate(vote):
+                ranks[char][idx] -= 1
+        
+        res = sorted([key for key in ranks.keys()])
+        res = sorted(res, key=lambda x:ranks[x])
+        return "".join(res)
+
+
 class Solution:
     def rankTeams(self, votes: List[str]) -> str:
         votedict = {char:[0]*len(votes[0]) for char in votes[0]}
