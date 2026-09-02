1cd3223de6b9a337e315324c95a7a1a416b7cf99
hogyeongkim <hogilkim@gmail.com>
2026-09-02 19:13:27 -0400

    Sep 2, 2026 125

--- 125. Valid Palindrome.py
+++ 125. Valid Palindrome.py
@@ -1,14 +1,27 @@
+# Sep 2, 2026
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = ""
+        for char in s:
+            temp_char = char.lower()
+            if "a" <= temp_char <= "z" or "0" <= temp_char <= "9":
+                new_s += temp_char
+
+        return new_s == new_s[::-1]
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         new_s = ""
 
         for char in s:
-            if 'a' <= char <= 'z' or 'A' <= char <= 'Z':
+            if "a" <= char <= "z" or "A" <= char <= "Z":
                 new_s += char.lower()
-            elif '0' <= char <= '9':
+            elif "0" <= char <= "9":
                 new_s += char
-        
+
         return new_s == new_s[::-1]
+
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
