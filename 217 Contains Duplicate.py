f79cdc883e45d519834bc0d77130a5e96a6a9c15
hogyeongkim <hogilkim@gmail.com>
2026-09-02 19:14:20 -0400

    Sep 2, 2026 217-4

--- 217 Contains Duplicate.py
+++ 217 Contains Duplicate.py
@@ -1,13 +1,22 @@
+# Sep 2, 2026 217-4
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))
+
+
 # Nov 14, 2023 217-3
+
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
 
 # solved Oct 30, 2022
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(nums) != len(set(nums))
+
 
 class Solution(object):
     def containsDuplicate(self, nums):
@@ -15,4 +24,3 @@ class Solution(object):
         if len(nums) > len(nums_set):
             return True
         return False
-        
