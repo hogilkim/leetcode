01c29cda3261c7d5dada0a89dc355853b8658eeb
hogyeongkim <hogilkim@gmail.com>
2026-09-03 17:27:33 -0400

    Sep 3, 2026 242-3

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,7 +1,31 @@
+# Sep 3, 2026 242-3
 import collections
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False
+        if len(s) != len(t):
+            return False
+        from collections import Counter
+
+        s_counter = Counter(s)
+        t_counter = Counter(t)
+
+        for key in s_counter:
+            if key not in s_counter:
+                return False
+            if s_counter[key] != t_counter[key]:
+                return False
+        return True
+
+
+import collections
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
 
         LEN = len(s)
         counter = collections.Counter()
@@ -9,13 +33,15 @@ class Solution:
         for i in range(LEN):
             counter[s[i]] += 1
             counter[t[i]] -= 1
-        
+
         for char in s:
-            if counter[char] != 0: return False
-        
+            if counter[char] != 0:
+                return False
+
         return True
 
-        #return collections.Counter(s) == collections.Counter(t)
+        # return collections.Counter(s) == collections.Counter(t)
+
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
