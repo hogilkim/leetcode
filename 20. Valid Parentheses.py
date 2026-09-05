c1e0404dbe7c57ae4df647228dd47141713a2874
hogyeongkim <hogilkim@gmail.com>
2026-09-05 10:56:36 -0400

    Sep 5, 2026 20-3

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -1,13 +1,33 @@
+# Sep 5, 2026 20-3
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        converter = {']':'[', '}': '{', ')': '('}
+        converter = {"]": "[", "}": "{", ")": "("}
         for char in s:
-            if char in "({[": stack.append(char)
-            elif not stack: return False
-            elif stack and stack[-1] == converter[char]: stack.pop()
-            else: return False
-        
+            if char in "[{(":
+                stack.append(char)
+            elif stack and converter[char] == stack[-1]:
+                stack.pop()
+            else:
+                return False
+
+        return not stack
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        converter = {"]": "[", "}": "{", ")": "("}
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            elif not stack:
+                return False
+            elif stack and stack[-1] == converter[char]:
+                stack.pop()
+            else:
+                return False
+
         return len(stack) == 0
 
 
@@ -15,26 +35,26 @@ class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
         for char in s:
-            if char == '(':
+            if char == "(":
                 stack.append(char)
-            elif char == ')':
-                if len(stack)>0 and stack[-1] == '(':
+            elif char == ")":
+                if len(stack) > 0 and stack[-1] == "(":
                     stack.pop()
                 else:
                     return False
-            elif char =='{':
+            elif char == "{":
                 stack.append(char)
-            elif char == '}':
-                if len(stack)>0 and stack[-1] == '{':
+            elif char == "}":
+                if len(stack) > 0 and stack[-1] == "{":
                     stack.pop()
                 else:
                     return False
-            elif char == '[':
+            elif char == "[":
                 stack.append(char)
-            elif char == ']':
-                if len(stack)>0 and stack[-1] == '[':
+            elif char == "]":
+                if len(stack) > 0 and stack[-1] == "[":
                     stack.pop()
                 else:
                     return False
-            
-        return len(stack) ==0
+
+        return len(stack) == 0
