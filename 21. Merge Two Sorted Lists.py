ed4d186e7cb19355cff020e738f236348312486a
hogyeongkim <hogilkim@gmail.com>
2026-09-08 17:45:51 -0400

    Sep 8, 2026 21-3

--- 21. Merge Two Sorted Lists.py
+++ 21. Merge Two Sorted Lists.py
@@ -1,8 +1,33 @@
-# solve again
-# Nov 6, 2023 
+# Sep 8, 2026 21-3
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        curr = head = ListNode()
+
+        while list1 and list2:
+            if list1.val > list2.val:
+                curr.next = list2
+                list2 = list2.next
+            else:
+                curr.next = list1
+                list1 = list1.next
+            curr = curr.next
+        if list1:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return head.next
+
+
+# solve again
+# Nov 6, 2023
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+
         dummy = ListNode()
         head = dummy
 
@@ -13,18 +38,19 @@ class Solution:
             elif not list2:
                 head.next = list1
                 list1 = list1.next
-            
+
             elif list1.val > list2.val:
                 head.next = list2
                 list2 = list2.next
-            
+
             else:
                 head.next = list1
                 list1 = list1.next
 
             head = head.next
-            
+
         return dummy.next
+
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -32,7 +58,9 @@ class Solution:
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    def mergeTwoLists(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
         dummy = ListNode()
         head = dummy
         while l1 or l2:
@@ -42,8 +70,8 @@ class Solution:
             elif not l2:
                 head.next = l1
                 l1 = l1.next
-            
-            elif l1.val> l2.val:
+
+            elif l1.val > l2.val:
                 head.next = l2
                 l2 = l2.next
             else:
