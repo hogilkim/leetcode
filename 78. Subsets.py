d486e7b7c37937c9661b66f7f46ff887a0bc92e7
hogyeongkim <hogilkim@gmail.com>
2026-09-11 13:10:56 -0400

    Sep 11, 2026 78-3

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -1,56 +1,78 @@
+# Sep 11, 2023 78-3
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtracking(i, subset):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            # not append curr element
+            backtracking(i + 1, subset)
+            # append curr element
+            subset.append(nums[i])
+            backtracking(i + 1, subset)
+            subset.pop()
+
+        backtracking(0, [])
+        return res
+
+
 # Dec 15, 2023 78-2
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
         def backtracking(i, part):
-            if i >= len(nums): 
+            if i >= len(nums):
                 res.append(part.copy())
                 return
 
             # X include i
-            backtracking(i+1, part.copy())
+            backtracking(i + 1, part.copy())
 
             # include i
             part.append(nums[i])
-            backtracking(i+1, part.copy())
+            backtracking(i + 1, part.copy())
 
-        
         backtracking(0, [])
 
         return res
+
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         # Another Solution & better, clean
         res = []
         subset = []
-        
+
         def backtrack(i):
             if i >= len(nums):
                 res.append(subset.copy())
                 return
-            
+
             # decision to include nums[i]
             subset.append(nums[i])
-            backtrack(i+1)
+            backtrack(i + 1)
             # decision not to include nums[i]
             subset.pop()
-            backtrack(i+1)
+            backtrack(i + 1)
+
         backtrack(0)
         return res
-        
+
         # my solution: worked
-        
+
+
 #         res = [[]]
-        
+
 #         def backtrack(part, i):
 #             if i >= len(nums) - 1:
 #                 return
 #             for j in range(i+1, len(nums)):
 #                 res.append(part + [nums[j]])
 #                 backtrack(part + [nums[j]], j)
-        
+
 #         backtrack([], -1)
-        
+
 #         return res
