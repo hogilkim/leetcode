fbc97aad16040292cfef301935300f271ab7e68c
hogyeongkim <hogilkim@gmail.com>
2026-09-11 06:56:40 -0400

    Sep 11, 2026 136-3

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -1,3 +1,13 @@
+# Sep 11, 2026 136-3
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            res ^= num
+
+        return res
+
+
 # Oct 30, 2023
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
@@ -6,18 +16,20 @@ class Solution:
             res ^= num
         return res
 
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
-        
-        return sum(list(set(nums)))*2 - sum(nums)
+
+        return sum(list(set(nums))) * 2 - sum(nums)
+
+
 #         seen = set()
-        
+
 #         for num in nums:
 #             if num not in seen:
 #                 seen.add(num)
 #             elif num in seen:
 #                 seen.remove(num)
-            
-        
+
+
 #         return list(seen).pop()
