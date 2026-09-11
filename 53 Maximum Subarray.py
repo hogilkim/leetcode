015fd200a874e50e0cfb31d10cd046bdc08de973
hogyeongkim <hogilkim@gmail.com>
2026-09-10 22:50:27 -0400

    Sep 10, 2026 53-4

--- 53 Maximum Subarray.py
+++ 53 Maximum Subarray.py
@@ -1,18 +1,37 @@
+# Sep 10, 2026 53-4
+# solve again
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_val = float("-inf")
+        currsum = 0
+
+        for num in nums:
+            currsum += num
+            max_val = max(max_val, currsum)
+            if currsum < 0:
+                currsum = 0
+
+        return max_val
+
+
 # Nov 17, 2023 53-3
 # solve again
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         currsum = 0
-        maxsum = float('-inf')
+        maxsum = float("-inf")
 
         for num in nums:
             currsum += num
             maxsum = max(maxsum, currsum)
-            if currsum < 0: currsum = 0
-        
+            if currsum < 0:
+                currsum = 0
+
         return maxsum
 
+
 # Second Attempt, looked at the answer.
+
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
@@ -23,5 +42,5 @@ class Solution:
             max_sum = max(current_sum, max_sum)
             if current_sum < 0:
                 current_sum = 0
-            
+
         return max_sum
