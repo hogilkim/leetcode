9640da476d3ebf8204eb2aca3ad94d2500fb604a
hogyeongkim <hogilkim@gmail.com>
2026-09-15 20:33:41 -0400

    Sep 15, 2026 3-4

--- 3 LongestSubstringWithoutRepeatingCharacters.py
+++ 3 LongestSubstringWithoutRepeatingCharacters.py
@@ -1,8 +1,26 @@
+# Sep 15, 2026 3-4
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charset = set()
+        prevlen = 0
+        maxlen = 0
+        l = 0
+        for r in range(len(s)):
+            char = s[r]
+            while char in charset and l < r:
+                charset.remove(s[l])
+                l += 1
+            charset.add(char)
+            maxlen = max(maxlen, len(charset))
+
+        return maxlen
+
+
 # Third attempt - solved
 # Nov 20, 2023 3-3
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+
         chars = set()
         l = 0
         max_len = 0
@@ -14,7 +32,7 @@ class Solution:
                 l += 1
             max_len = max(max_len, r - l + 1)
             chars.add(s[r])
-        
+
         return max_len
 
 
@@ -22,26 +40,27 @@ class Solution:
 # second attempt - Jan 15, 2022
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if not s: return 0
+        if not s:
+            return 0
         l, r = 0, 1
-        
+
         substrings = set([s[0]])
         max_len = 1
-        
+
         while r < len(s):
             if s[r] not in substrings:
                 substrings.add(s[r])
             else:
                 while s[l] != s[r]:
                     substrings.remove(s[l])
-                    l+=1
+                    l += 1
                 l += 1
-            max_len = max(max_len, r-l+1)
-            r+=1
-            
-            
-        
+            max_len = max(max_len, r - l + 1)
+            r += 1
+
         return max_len
+
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
@@ -49,17 +68,17 @@ class Solution(object):
         :rtype: int
         """
         longest_len = 0
-        
+
         current_substring = ""
         current_len = 0
         for element in s:
             if element in current_substring:
-                longest_len=max(current_len, longest_len)
+                longest_len = max(current_len, longest_len)
                 index = current_substring.index(element)
                 current_substring += element
-                current_substring = current_substring[index+1:]
+                current_substring = current_substring[index + 1 :]
                 current_len = len(current_substring)
-                
+
             else:
                 current_substring += element
                 current_len = len(current_substring)
