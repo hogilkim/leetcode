954cd5ff7e74dd62d3ffc8659f9b2966e0f5b26f
hogyeongkim <hogilkim@gmail.com>
2026-09-15 20:55:18 -0400

    Sep 15, 2026 133-4

--- 133. Clone Graph.py
+++ 133. Clone Graph.py
@@ -1,8 +1,29 @@
+# Sep 15, 2026 133-4
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return None
+        hashmap = {}  # key: node.val value: node
+
+        def dfs(node):
+            if node.val in hashmap:
+                return hashmap[node.val]
+            copy = Node(val=node.val)
+            hashmap[copy.val] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+
+        return dfs(node)
+
+
 # Nov 25, 2023 133-3 solve again
 from typing import Optional
+
+
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+
         og_to_clone = {}
 
         def dfs(n):
@@ -13,18 +34,18 @@ class Solution:
             og_to_clone[n] = copy
             for nei in n.neighbors:
                 copy.neighbors.append(dfs(nei))
-            
+
             return copy
-        
+
         return dfs(node) if node else None
-    
+
 
 # second try, couldn't solve
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        
+    def cloneGraph(self, node: "Node") -> "Node":
+
         og_to_clone = {}
-        
+
         def dfs(node):
             if node in og_to_clone:
                 return og_to_clone[node]
@@ -32,23 +53,26 @@ class Solution:
             og_to_clone[node] = copy
             for nei in node.neighbors:
                 copy.neighbors.append(dfs(nei))
-            
+
             return copy
+
         return dfs(node) if node else None
+
 
 # Solve again
 
+
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        og_to_copy={}
+    def cloneGraph(self, node: "Node") -> "Node":
+        og_to_copy = {}
 
         def dfs(node):
             if node in og_to_copy:
                 return og_to_copy[node]
             copy_node = Node(node.val)
-            og_to_copy[node]=copy_node
+            og_to_copy[node] = copy_node
             for neighbor in node.neighbors:
                 copy_node.neighbors.append(dfs(neighbor))
             return copy_node
-        
+
         return dfs(node) if node else None
