6f253b8b4c84c5e74b09a9f43ddf1bd1315ef3fc
hogyeongkim <hogilkim@gmail.com>
2026-09-20 07:38:17 -0400

    Sep 20, 2026 199-3

--- 199. Binary Tree Right Side View.py
+++ 199. Binary Tree Right Side View.py
@@ -1,21 +1,48 @@
+# Sep 20, 2026 199-3
+class Solution:
+    def rightSideView(self, root: TreeNode | None) -> list[int]:
+        from collections import deque
+
+        if not root:
+            return []
+        queue = deque([root])
+        res = []
+        while queue:
+            res.append(queue[-1].val)
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return res
+
+
 # Dec 11, 2023 199-2
 from collections import deque
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         queue = deque([])
-        if root: queue.append(root)
+        if root:
+            queue.append(root)
         res = []
 
         while queue:
-            
+
             res.append(queue[-1].val)
 
             for _ in range(len(queue)):
                 node = queue.popleft()
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
-        
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
         return res
+
 
 # solved
 # first attempt - Jan 17, 2022
@@ -26,18 +53,23 @@ class Solution:
 #         self.left = left
 #         self.right = right
 import collections
+
+
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        
+
         deque = collections.deque([])
-        if root: deque.append(root)
-            
+        if root:
+            deque.append(root)
+
         result = []
         while deque:
             result.append(deque[-1].val)
             for i in range(len(deque)):
                 curr = deque.popleft()
-                if curr.left: deque.append(curr.left)
-                if curr.right: deque.append(curr.right)
-        
+                if curr.left:
+                    deque.append(curr.left)
+                if curr.right:
+                    deque.append(curr.right)
+
         return result
