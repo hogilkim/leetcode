b5b8736d8f206dd853cc6f1ecf21261902239024
hogyeongkim <hogilkim@gmail.com>
2026-09-20 22:08:03 -0400

    Sep 20, 2026 143-3

--- 143. Reorder List.py
+++ 143. Reorder List.py
@@ -1,3 +1,47 @@
+# Sep 20, 2026 143-3
+class Solution:
+    def reorderList(self, head: ListNode | None) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        #   s f
+        # 1 2 3 4
+
+        #     s   f
+        # 1 2 3 4 5
+
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # if fast.next:
+        #     fast = fast.next
+        latter_head = slow.next
+
+        # reverse second half
+        # 3 4 5
+        # p h
+        prev = None
+        slow.next = None
+        while latter_head:
+            print(latter_head.val)
+            nxt = latter_head.next
+            latter_head.next = prev
+            prev = latter_head
+            latter_head = nxt
+
+        first, second = head, prev
+        while second:
+            first_next = first.next
+            second_next = second.next
+            first.next = second
+            second.next = first_next
+
+            first = first_next
+            second = second_next
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -12,17 +56,18 @@
 
 # memorize fast = head.next
 
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
         slow, fast = head, head.next
-        
+
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-        
+
         # reverse second half
         curr = slow.next
         prev = slow.next = None
@@ -31,36 +76,32 @@ class Solution:
             curr.next = prev
             prev = curr
             curr = nxt
-            
-        
+
         first, second = head, prev
         while second:
             first_next = first.next
             second_next = second.next
             first.next = second
             second.next = first_next
-            
+
             first = first_next
             second = second_next
-                    
 
-        
         return head
-            
+
 
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        
+
         # second solution
         slow, fast = head, head.next
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
-        
-        
+
         second_half_ptr = slow.next
         second_half_ptr_prev = slow.next = None
         while second_half_ptr:
@@ -68,7 +109,7 @@ class Solution:
             second_half_ptr.next = second_half_ptr_prev
             second_half_ptr_prev = second_half_ptr
             second_half_ptr = temp
-        
+
         right_node = second_half_ptr_prev
         left_node = head
         while right_node:
@@ -77,20 +118,22 @@ class Solution:
             left_node.next = right_node
             right_node.next = left_next
             right_node, left_node = right_next, left_next
-                    
-        #first solution
+
+        # first solution
+
+
 #         node_lists = []
 #         curr_node = head
 #         while curr_node:
 #             node_lists.append(curr_node)
 #             curr_node = curr_node.next
-        
+
 #         added = set()
-#         l = 0 
+#         l = 0
 #         r = len(node_lists)-1
-        
+
 #         dummy = ListNode(0)
-        
+
 #         while l <= r:
 #             dummy.next = node_lists[l]
 #             dummy = dummy.next
