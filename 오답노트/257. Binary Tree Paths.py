cd2282727b09f75ff0d215348598c0d332d6a0c7
hogyeongkim <hogilkim@gmail.com>
2026-09-24 09:39:42 -0400

    Sep 24, 2026 257

---
+++ 오답노트/257. Binary Tree Paths.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: TreeNode | None) -> list[str]:
+        res = []
+
+        def dfs(node, path):
+            if not node.left and not node.right:
+                path += str(node.val)
+                res.append(path)
+            if node.left:
+                dfs(node.left, path + str(node.val) + "->")
+            if node.right:
+                dfs(node.right, path + str(node.val) + "->")
+
+        dfs(root, "")
+        return res
