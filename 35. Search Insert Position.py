3039b25946c9750a19a14394ad6006d767469e01
hogyeongkim <hogilkim@gmail.com>
2026-09-24 09:35:00 -0400

    Sep 24, 2026 35-2

--- 35. Search Insert Position.py
+++ 35. Search Insert Position.py
@@ -1,13 +1,31 @@
 class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if target == nums[mid]:
+                return mid
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+
+        return l
+
+
+class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums)
-        
-        while l<r:
-            mid = (l+r)//2
-            
+
+        while l < r:
+            mid = (l + r) // 2
+
             if nums[mid] >= target:
                 r = mid
-            
-            else: l = mid + 1
-        
+
+            else:
+                l = mid + 1
+
         return l
