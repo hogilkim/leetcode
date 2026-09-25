c6554ce6850f1b9f36c1b46ebe2c790142d9b2e5
hogyeongkim <hogilkim@gmail.com>
2026-09-24 22:38:40 -0400

    Sep 24, 2026 45-2

--- 45. Jump Game II.py
+++ 45. Jump Game II.py
@@ -1,13 +1,33 @@
+# solve again
+# Sep 24, 2026 45-2
+class Solution:
+    def jump(self, nums: list[int]) -> int:
+        steps = 0
+        l, r = 0, 0
+
+        while r < len(nums) - 1:
+
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+
+            l = r + 1
+            r = farthest
+            steps += 1
+
+        return steps
+
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
         steps = 0
         curr_end = 0
         curr_furthest = 0
-        for i in range(len(nums)-1):
+        for i in range(len(nums) - 1):
             curr_furthest = max(curr_furthest, nums[i] + i)
 
-            if curr_furthest >= len(nums)-1:
+            if curr_furthest >= len(nums) - 1:
                 steps += 1
                 break
 
@@ -15,8 +35,7 @@ class Solution:
                 steps += 1
                 curr_end = curr_furthest
 
-        return steps                
-
+        return steps
 
         # dp solution w/ time O(n) - O(n^2)
 
@@ -27,5 +46,5 @@ class Solution:
 
         #     for j in range(i, min(i+nums[i]+1, len(nums))):
         #         dp[j] = min(dp[j],dp[i] + 1)
-        
+
         # return dp[-1]
