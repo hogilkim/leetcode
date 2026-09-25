ac27cee80bf6b8dbe692240b6fef73eff21b312a
hogyeongkim <hogilkim@gmail.com>
2026-09-25 13:49:16 -0400

    Sep 25, 2026 230-3

--- 230. Kth Smallest Element in a BST.py
+++ 230. Kth Smallest Element in a BST.py
@@ -1,3 +1,27 @@
+# Sep 25, 2026 230-3
+class Solution:
+    def kthSmallest(self, root: TreeNode | None, k: int) -> int:
+        res = 0
+
+        def dfs(node, seq, k):
+            nonlocal res
+
+            cumul = seq
+            if node.left:
+                cumul = dfs(node.left, cumul, k)
+            cumul += 1
+            if cumul == k:
+                res = node.val
+
+            if node.right:
+                cumul = dfs(node.right, cumul, k)
+
+            return cumul
+
+        dfs(root, 0, k)
+        return res
+
+
 # Dec 20, 2023 230-2
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
@@ -5,26 +29,29 @@ class Solution:
 
         def dfs(node, acc):
             nonlocal res
-            if not node: return 0
+            if not node:
+                return 0
 
             left = dfs(node.left, acc)
-            if left + acc + 1 == k: res = node.val
+            if left + acc + 1 == k:
+                res = node.val
             right = dfs(node.right, left + acc + 1)
-            
+
             return left + 1 + right
-        
+
         dfs(root, 0)
         return res
+
 
 # solve again
 # second attempt - Jan 13, 2022
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        n = 0 
+
+        n = 0
         stack = []
         curr = root
-        
+
         while curr or stack:
             while curr:
                 stack.append(curr)
@@ -34,15 +61,13 @@ class Solution:
             if n == k:
                 return curr.val
             curr = curr.right
-        
-
 
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        return sorted(self.helper(root, []))[k-1]
-    
+
+        return sorted(self.helper(root, []))[k - 1]
+
     def helper(self, node, num_list):
         if node:
             num_list.append(node.val)
